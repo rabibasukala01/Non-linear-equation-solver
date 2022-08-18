749628efdc37bf0b85eb46ef717592cca591a81a
rabibasukala <ibarbasukala77@gmail.com>
2022-08-18 15:23:28 +0545

    added falsePositionMethod

---
+++ Bracketting method/false_position_method.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+#define f(x) pow(x, 3) - 4 * x - 9 // your equation here
+
+int main()
+{
+    float x0, x1, x2, f0, f1, f2, error = 0.001;
+// Initial guessess
+guess:
+    cout << "Enter First initial guess : ";
+    cin >> x0;
+    cout << "Enter second initial guess : ";
+    cin >> x1;
+
+    f0 = f(x0);
+    f1 = f(x1);
+
+    // checking if guesses brackets the root
+    if ((f0 * f1) > 0)
+    {
+        cout << "Your guesses arenot in bracket.Please enter again\n";
+        goto guess;
+    }
+    else
+    {
+        cout << "Enter error tolorence : ";
+        cin >> error;
+        do
+        {
+            x2 = x0 - ((f0 * (x1 - x0)) / (f1 - f0));
+            f2 = f(x2);
+
+            if (f2 * f0 < 0)
+            {
+                x1 = x2;
+                f1 = f2;
+            }
+            else
+            {
+                x0 = x2;
+                f0 = f2;
+            }
+
+        } while (fabs(f2) > error);
+    }
+
+    cout << "Root of f(x) with error tolorence " << error << " is " << x2;
+
+    return 0;
+}
