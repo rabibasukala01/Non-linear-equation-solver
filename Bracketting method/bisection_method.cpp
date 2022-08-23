2ffcb20858316e5ae5d29f47955091b5fe972c34
Rabi Basukala <63946883+rabibasukala01@users.noreply.github.com>
2022-08-23 20:38:56 +0545

    added table format

--- Bracketting method/bisection_method.cpp
+++ Bracketting method/bisection_method.cpp
@@ -24,8 +24,22 @@ guess:
     }
     else
     {
+        x2 = (x0 + x1) / 2.0;
+        f2 = f(x2);
         cout << "Enter error tolorence : ";
         cin >> error;
+        cout << "x0"
+             << "\t\t"
+             << "x1"
+             << "\t\t"
+             << "x2"
+             << "\t\t"
+             << "f0"
+             << "\t\t"
+             << "f1"
+             << "\t\t"
+             << "f2" << endl;
+        cout << x0 << "\t\t" << x1 << "\t\t" << x2 << "\t\t" << f0 << "\t\t" << f1 << "\t\t" << f2 << endl;
         do
         {
             x2 = (x0 + x1) / 2.0;
@@ -41,6 +55,7 @@ guess:
                 x0 = x2;
                 f0 = f2;
             }
+            cout << x0 << "\t\t" << x1 << "\t\t" << x2 << "\t\t" << f0 << "\t\t" << f1 << "\t\t" << f2 << endl;
         } while (fabs(f2) > error);
     }
     cout << "Root of f(x) with error tolorence " << error << " is " << x2;
