581115d789da64deb4cec6cbaf01e32342895fcb
Rabi Basukala <63946883+rabibasukala01@users.noreply.github.com>
2022-08-23 22:33:22 +0545

    added table format

--- Non-Bracketting method/Newton_Rapshon_method.cpp
+++ Non-Bracketting method/Newton_Rapshon_method.cpp
@@ -6,17 +6,25 @@ using namespace std;
 int main()
 {
     float x0, deri, f0, error;
+    int i = 0;
     // only one Initial guess
-    cout << "Enter a initial guess : ";
+    cout
+        << "Enter a initial guess : ";
     cin >> x0;
     cout << "Enter error tolorence : ";
     cin >> error;
-
+    cout << "i"
+         << "\t\t\t"
+         << "x0 "
+         << "\t\t\t"
+         << "f0" << endl;
     do
     {
         f0 = f(x0);
         deri = d(x0);
         x0 = x0 - (f0 / deri);
+        i = i + 1;
+        cout << i << "\t\t\t" << x0 << "\t\t\t" << f0 << endl;
 
     } while (fabs(f0) > error);
 
